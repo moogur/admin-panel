716c2c21fbb22a0c20e540ae5cf6c29503dffec7
Alexey <moogur-mail@yandex.ru>
2023-07-02 20:27:42 +0300

    [AUTH-296] fix build

--- src/pages/MainPage/constants.ts
+++ src/pages/MainPage/constants.ts
@@ -7,19 +7,23 @@ import { Service } from './MainPageTypes';
 export const columns: ColumnsType<Service> = [
   {
     title: 'Name',
+    key: 'name',
     dataIndex: 'name',
   },
   {
     title: 'Link to swagger',
+    key: 'swagger',
     dataIndex: 'swagger',
     customRender: (value: string) => h('a', { href: value, target: '_blank', rel: 'noreferrer' }, value),
   },
   {
     title: 'Version',
+    key: 'version',
     dataIndex: 'version',
   },
   {
     title: 'IP',
+    key: 'ip',
     dataIndex: 'ip',
   },
 ];
